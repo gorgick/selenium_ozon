5fe7d9e2ba8f59c599aadc3871f0255bfd10cf7e
gorgick <gorgick2014@yandex.by>
2026-01-30 20:47:50 +0300

    init_webdriver

---
+++ init_driver.py
@@ -0,0 +1,19 @@
+import time
+
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium_stealth import stealth
+
+
+def init_webdriver():
+    driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+    stealth(driver,
+            languages=["en-US", "en"],
+            vendor="Google Inc.",
+            platform="Win32",
+            webgl_vendor="Intel Inc.",
+            renderer="Intel Iris OpenGL Engine",
+            fix_hairline=True,
+            )
+    return driver
