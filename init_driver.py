a437b3e8b90055bc0d717c81333dd80d950904aa
gorgick <gorgick2014@yandex.by>
2026-02-03 16:13:04 +0300

    finded name&url

--- init_driver.py
+++ init_driver.py
@@ -1,5 +1,5 @@
 import time
-
+from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
@@ -21,13 +21,29 @@ def init_webdriver():
 
 def scrolldown(driver, deep):
     for _ in range(deep):
-        driver.execute_script('window.scrollBy(0, 500)')
+        driver.execute_script('window.scrollBy(0, 200)')
         time.sleep(0.1)
 
 
 def get_mainpage_cards(driver, url):
     driver.get(url)
     scrolldown(driver, 50)
+    main_page_html = BeautifulSoup(driver.page_source, "html.parser")
+    content = main_page_html.find("div", {"class": "container c"})
+    content = content.find_all(recursive=False)[-1].find("div")
+    content = content.find_all(recursive=False)
+    content = [item for item in content if "island" in str(item)][-1]
+    content = content.find("div").find("div").find("div")
+    content = content.find_all(recursive=False)
+
+    for layer in content:
+        layer = layer.find("div")
+        cards = layer.find_all(recursive=False)
+        for card in cards:
+            card = card.find_all(recursive=False)
+            card_name = card[2].find("span", {"class": "tsBody500Medium"}).contents[0]
+            card_url = card[2].find("a", href=True)["href"]
+            print(card_name, card_url)
 
 
 if __name__ == "__main__":
@@ -35,4 +51,3 @@ if __name__ == "__main__":
     get_mainpage_cards(driver, "https://ozon.by")
     time.sleep(25)
     driver.quit()
-
